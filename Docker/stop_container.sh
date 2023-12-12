f3c035dfeabead297ffd478cc117b1a287f1c980
michalsadura1 <michalsadura4@gmail.com>
2023-12-12 12:13:45 +0100

    fix scripts

--- Docker/stop_container.sh
+++ Docker/stop_container.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-set -x
+#set -x
 set -o errexit
 set -o errtrace
 set -o nounset
 set -o pipefail
-echo "Stop all docker conatiner"
-docker ps -q | xargs docker stop
-
+echo "!!! Stop all running docker conatiners !!!"
+docker ps -a -q -f status=running | xargs docker stop
